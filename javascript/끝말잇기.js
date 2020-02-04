e33e53506e540cd9d6812c59996e6afa08bcc36e
MoonJungHyeok <wjdgur9534@daum.net>
2020-02-04 23:03:56 +0900

    끝말잇기

---
+++ javascript/끝말잇기.js
@@ -0,0 +1,11 @@
+var word=prompt('첫단어 입력하세요')
+while(true){
+    var newword=prompt(word);
+    if(word[word.length-1]==newword[0]){
+        word=newword;
+    }
+    else{
+        console.log('다시입력하세요')
+    }
+}
+
